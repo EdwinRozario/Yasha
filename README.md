4c0cedd9f59e88427d6a7ce9fc9a1905bdbdb8a1
edwin <edwin@edwin-Inspiron-1525.(none)>
2012-05-22 15:58:49 +0530

    Update with index and condition added to yasha

--- README.md
+++ README.md
@@ -8,3 +8,10 @@ Enables to make an SQL like data structure using Redis. And use it like an SQL l
 #Requirements: 
 <tt>redis(2.2.2)</tt>
 <tt>json(1.5.3)</tt>
+
+#Usage:
+
+#Note:
+1. Database and tables for Yasha can be made with Yasha only.
+2. For every table entry Yasha will provide an index. Object.select(:index => 5).index = 5
+3. Dont name any fields in table as index. All the table will have a default field index.
--- lib/yasha.rb
+++ lib/yasha.rb
@@ -90,7 +90,6 @@ class Yasha
 ###INSERT###
 
   def self.insert row
-
     row.keys.each do |field|
       return "#{field} not in table." if not @table_struct.include? field
     end    
@@ -99,52 +98,54 @@ class Yasha
       row[field] = nil if not row.keys.include? field 
     end
 
-    row_id = @redis_connection.get(@counter_key)
+    row_index = @redis_connection.get(@counter_key)
 
     row.each do |key, value|
-      yasha_key = "YashA:#{@database_id}:#{@table_id}:#{@table_struct.index(key)}:#{value}:#{row_id}"
-      @redis_connection.set(yasha_key, row_id)
+      yasha_key = "YashA:#{@database_id}:#{@table_id}:#{@table_struct.index(key)}:#{value}:#{row_index}"
+      @redis_connection.set(yasha_key, row_index)
     end
     
-    @redis_connection.set("YashA:Row:#{@database_id}:#{@table_id}:#{row_id}", row.to_json)
+    @redis_connection.set("YashA:Row:#{@database_id}:#{@table_id}:#{row_index}", row.to_json)
     @redis_connection.incr(@counter_key)
   end
 
 ###SELECT###
 
   def self.select_by_condition key, value, limit = nil
-    result_ids = []
+    result_indexs = []
     rows = @redis_connection.keys("YashA:#{@database_id}:#{@table_id}:#{@table_struct.index(key)}:#{value}:*")
     rows = rows.slice(0, limit) if not limit.nil?
     rows.each do |row|
-      result_ids << @redis_connection.get(row)
+      result_indexs << @redis_connection.get(row)
     end
-    return result_ids
+    return result_indexs
   end
 
-  def self.select_by_conditions query, limit = nil
-    result_ids = []
+  def self.select_by_conditions query, limit = nil, internal = nil
+    result_indexs = []
 
     query.each do |key, value|
-      condition_ids = self.select_by_condition(key, value, limit)     
-      result_ids = result_ids.empty? ? condition_ids : result_ids & condition_ids
-      return [] if result_ids.empty? or condition_ids.empty?
+      condition_indexs = self.select_by_condition(key, value, limit)
+      result_indexs = result_indexs.empty? ? condition_indexs : result_indexs & condition_indexs
+      return nil if result_indexs.empty? or condition_indexs.empty?
     end
     
     results = []
 
-    result_ids.uniq.each do |id|
-      row = JSON.parse(@redis_connection.get("YashA:Row:#{@database_id}:#{@table_id}:#{id}"))
-      results << OpenStruct.new(row)
+    result_indexs.uniq.each do |index|
+      row = JSON.parse(@redis_connection.get("YashA:Row:#{@database_id}:#{@table_id}:#{index}"))
+      row["index"] = index
+      results << (internal.nil? ? OpenStruct.new(row) : row)
     end
 
     return results
 
   end
 
-  def self.select_by_id id
-    return nil if id > @redis_connection.get(@counter_key).to_i - 1 or id < 0
-    row = JSON.parse(@redis_connection.get("YashA:Row:#{@database_id}:#{@table_id}:#{id}"))
+  def self.select_by_index index
+    return nil if index > @redis_connection.get(@counter_key).to_i - 1 or index < 0
+    row = JSON.parse(@redis_connection.get("YashA:Row:#{@database_id}:#{@table_id}:#{index}"))
+    row["index"] = index
     return OpenStruct.new(row)
   end
 
@@ -153,6 +154,7 @@ class Yasha
 
     number.times do |id|
       row = JSON.parse(@redis_connection.get("YashA:Row:#{@database_id}:#{@table_id}:#{id}"))
+      row["index"] = id
       result_list << OpenStruct.new(row)      
     end
 
@@ -169,11 +171,75 @@ class Yasha
     return self.select_all if query.nil?
 
     case
-    when (query.has_key? :id) then return self.select_by_id(query[:id])
+    when (query.has_key? :index) then return self.select_by_index(query[:index])
     when (query.has_key? :limit and not query.has_key? :conditions) then return self.select_all(query[:limit])
     when (query.has_key? :limit and query.has_key? :conditions) then return self.select_by_conditions(query[:conditions], query[:limit])
     when (not query.has_key? :limit and query.has_key? :conditions) then return self.select_by_conditions(query[:conditions])
     else return nil
+    end
+
+  end
+
+###UPDATE###
+
+  def self.update_index index, updation
+    rows = @redis_connection.keys("YashA:#{@database_id}:#{@table_id}:*:*:#{index}")
+    rows.each do |row|
+      @redis_connection.del(row)
+    end
+   
+    row_data = JSON.parse(@redis_connection.get("YashA:Row:#{@database_id}:#{@table_id}:#{index}"))
+    row_data.each do |key, value|
+      row_data[key] = updation[key]  if updation.keys.include? key
+    end
+    @redis_connection.del("YashA:Row:#{@database_id}:#{@table_id}:#{index}")
+
+
+    row_data.each do |key, value|
+      new_key = "YashA:#{@database_id}:#{@table_id}:#{@table_struct.index(key)}:#{value}:#{index}"
+      @redis_connection.set(new_key, index)
+    end
+
+    @redis_connection.set("YashA:Row:#{@database_id}:#{@table_id}:#{index}", row_data.to_json)
+
+  end
+
+  def self.update_row row, updation
+
+    row_index = row["index"]
+    row.delete("index")
+    updated_row = updation
+
+    row.each do |key, value|
+      if updation.keys.include? key
+        yasha_key = "YashA:#{@database_id}:#{@table_id}:#{@table_struct.index(key)}:#{value}:#{row_index}"
+        @redis_connection.del(yasha_key)
+      else
+        updated_row[key] = value
+      end
+    end
+
+    updation.each do |key, value|
+      yasha_key = "YashA:#{@database_id}:#{@table_id}:#{@table_struct.index(key)}:#{value}:#{row_index}"
+      @redis_connection.set(yasha_key, row_index)
+    end
+    
+    @redis_connection.set("YashA:Row:#{@database_id}:#{@table_id}:#{row_index}", updated_row.to_json)
+      
+  end
+
+  def self.update updation
+
+    if updation.has_key? :index
+      self.update_index updation[:index], updation[:set]
+    else
+      return false if updation[:conditions].nil?
+      rows = self.select_by_conditions(updation[:conditions], nil, 0)
+      return false if rows.nil?
+
+      rows.each do |row|
+        self.update_row(row, updation[:set])
+      end
     end
 
   end
@@ -215,11 +281,15 @@ Job.details
 #Job.insert({"name" => "Patton", "alias" => "TheTank", "nationality" => "USA"}) #WF
 #Job.insert({"name" => "Fredrik Paulo", "alias" => "Barbarosa", "nationality" => "German"}) #WF
 
-#Job.select(:id => 5).name #WF
+#Job.select(:index => 5) #WF
 #Job.select #WF
 #Job.select(:limit => 6) #WF
 #Job.select(:conditions => {"name" => "Patton"}) #WF
 #Job.select(:conditions => {"nationality" => "German"}, :limit => 2) #WF
-#Job.select(:conditions => {"nationality" => "German", "name" => "*o*", "alias" => "*t*"}) #WF
+#Job.select(:conditions => {"nationality" => "German", "name" => "*o*", "alias" => "*t*") #WF
 
+#Job.update(:set => {"name" => "WalterModel", "alias" => "BulgeBat"}, :conditions => {"name" => "Fermanshtine"}) #WF
+#Job.update(:set => {"name" => "Fermanchtine", "alias" => "BerlinGuard", "nationality" => "German"}, :index => 2) #WF
+
+#Job.delete(:condition => {}, :id => 1)
 ###############
